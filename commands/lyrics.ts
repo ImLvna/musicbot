6fb39187fc3d670ee411d99b73950ff3b5bdf2fc
Luna <imlvnaa@gmail.com>
2023-07-20 17:16:54 -0400

    switch to using our own html scraping for lyrics

--- commands/lyrics.ts
+++ commands/lyrics.ts
@@ -1,40 +1,65 @@
 import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
-import { getLyrics } from "genius-lyrics-api";
 import { i18n } from "../utils/i18n";
 import { config } from "../utils/config";
-
-
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 import { bot } from "../index";
 
+axios.defaults.headers.common["Authorization"] = `Bearer ${config.GENIUS_ACCESS_TOKEN}`;
+axios.defaults.baseURL = "https://api.genius.com";
 
 export default {
-  data: new SlashCommandBuilder().setName("lyrics").setDescription(i18n.__("lyrics.description"))
-  .addStringOption((option) => option.setName("song").setDescription("The song name").setRequired(false))
-  .addStringOption((option) => option.setName("artist").setDescription("The artist name").setRequired(false)),
+  data: new SlashCommandBuilder()
+    .setName("lyrics")
+    .setDescription(i18n.__("lyrics.description"))
+    .addStringOption((option) => option.setName("song").setDescription("The song name").setRequired(false))
+    .addStringOption((option) => option.setName("artist").setDescription("The artist name").setRequired(false)),
   async execute(interaction: ChatInputCommandInteraction) {
-    if (!config.GENIUS_ACCESS_TOKEN || config.GENIUS_ACCESS_TOKEN=== "") return interaction.reply(i18n.__("lyrics.errorNotConfigured")).catch(console.error);
+    if (!config.GENIUS_ACCESS_TOKEN || config.GENIUS_ACCESS_TOKEN === "")
+      return interaction.reply(i18n.__("lyrics.errorNotConfigured")).catch(console.error);
     const queue = bot.queues.get(interaction.guild!.id);
 
-    if ((!queue || !queue.songs.length) && !interaction.options.getString('song') && !interaction.options.getString('artist')) return interaction.reply(i18n.__("lyrics.errorNotQueue")).catch(console.error);
+    if (
+      (!queue || !queue.songs.length) &&
+      !interaction.options.getString("song") &&
+      !interaction.options.getString("artist")
+    )
+      return interaction.reply(i18n.__("lyrics.errorNotQueue")).catch(console.error);
 
     await interaction.reply("⏳ Loading...").catch(console.error);
 
     let lyrics = null;
-    let title: string = interaction.options.getString('song') || queue?.songs[0].title || ' ';
-    let artist: string = interaction.options.getString('artist') || ' '
+    let title = interaction.options.getString("song") || queue?.songs[0].title || " ";
+    let artist = interaction.options.getString("artist");
 
-    if (title === " ") return interaction.reply(i18n.__("lyrics.errorNotQueue")).catch(console.error)
+    if (title === " ") return interaction.reply(i18n.__("lyrics.errorNotQueue")).catch(console.error);
 
     try {
-      const options = {
-        apiKey: config.GENIUS_ACCESS_TOKEN,
-        title: title,
-        artist: artist,
-        optimizeQuery: true
-      };
+      const songList = await axios.get("/search", {
+        params: {
+          q: `${title}${artist ? ` ${artist}` : ""}`
+        }
+      });
 
-      lyrics = await getLyrics(options);
+      title = `${songList.data.response.hits[0].result.title} - ${songList.data.response.hits[0].result.primary_artist.name}`;
+
+      const html = await axios.get(songList.data.response.hits[0].result.url);
+
+      const $ = cheerio.load(html.data);
+
+      const lyricsContainer = $('[data-lyrics-container="true"]');
+
+      // Replace <br> with newline
+      $("br", lyricsContainer).replaceWith("\n");
+
+      // Replace the elements with their text contents
+      $("a", lyricsContainer).replaceWith((_i, el) => $(el).text());
+
+      // Remove all child elements, leaving only top-level text content
+      lyricsContainer.children().remove();
+
+      lyrics = lyricsContainer.text();
 
       if (!lyrics) lyrics = i18n.__mf("lyrics.lyricsNotFound", { title: title });
     } catch (error) {
